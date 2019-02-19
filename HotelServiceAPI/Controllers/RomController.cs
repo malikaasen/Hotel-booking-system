6f05408f40593bf0bfc16c3d4bf87521b93f1c36
OysteinKnudsen <oysteinknudsen96@gmail.com>
2019-02-19 22:16:12 +0100

    Added endpoint for Rom, working proof of concept

---
+++ HotelServiceAPI/Controllers/RomController.cs
@@ -0,0 +1,31 @@
+﻿using HotelServiceAPI.Models;
+using SQLConnectionApplication.DataProviders;
+using SQLConnectionApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HotelServiceAPI.Controllers
+{
+    [RoutePrefix("Rom")]
+    public class RomController : ApiController
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult HentAlleRom()
+        {
+            RomDataprovider romDataprovider = new RomDataprovider();
+            List<RomDTO> alleRom = romDataprovider.FinnAlleRom().Select(RomDTO.CreateFrom).ToList();
+            if (alleRom.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(alleRom);
+        }
+    }
+}
---
+++ HotelServiceAPI/Models/Rom.cs
@@ -0,0 +1,27 @@
+﻿using SQLConnectionApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelServiceAPI.Models
+{
+    public class RomDTO
+    {
+        public int RomID { get; set; }
+        public Storrelse Storrelse { get; set; }
+        public Kvalitet Kvalitet { get; set; }
+        public int AntallSenger { get; set; }
+
+        internal static RomDTO CreateFrom(Rom rom)
+        {
+            return new RomDTO
+            {
+                RomID = rom.RomID,
+                Storrelse = rom.Storrelse,
+                Kvalitet = rom.Kvalitet,
+                AntallSenger = rom.AntallSenger
+            };
+        }
+    }
+}
