4e1f4c2c0e4906927c4b9d0715dd5f12ca7c0a89
Magnus Ødegård Bergersen <181182@stud.hvl.no>
2019-02-25 19:20:01 +0100

    Lagt til RomReservasjon

--- OnlineBookingWeb/Controllers/RomReservasjonController.cs
+++ OnlineBookingWeb/Controllers/RomReservasjonController.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using SQLConnectionApplication.DataProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ApplicationServices;
 using System.Web.Mvc;
 
 namespace OnlineBookingWeb.Controllers
 {
     public class RomReservasjonController : Controller
     {
+		RomDataprovider romProvider = new RomDataprovider();
         // GET: RomReservasjon
         public ActionResult Index()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public ActionResult FinnLedigRom(DateTime fdato, DateTime tdato)
+		{
+			var model = romProvider.FinnAlleRom();
+			return View(model);
+		}
+	}
+
 }
